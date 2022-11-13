b5ac2101a214537c9e74afa256c6b2ba171d81d0
HutchesonJoe <hutcheson2001@yahoo.com>
2022-11-13 22:59:27 +0000

    mixed lists

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
-my_list = [ "192.168.0.5", 5060, "UP" ]
-#display the first item on the list
+
+#create a list containing three items
+my_list = ["192.168.0.5", 5060, "UP"]
+
+#return the first item in the list
 print("The first item in the list (IP): " + my_list[0])
-#display the second item on the list
+
+#return the second item in the list
 print("The second item in the list (port): " + str(my_list[1]))
-#display the last item on the list
-print("The last item in the list (state): " + my_list[2])
+
+#return the third item on the list
+print("The third item on the list (state): " + my_list[2])
+
 iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
-#display only the IP address
-print("IP addresses: " + iplist[3] + " and"  + iplist[4])
-print("IP addresses: ", iplist[3], " and", iplist[4])
+
+print("IP addresses: " + iplist[3] + ", and " + iplist[4])
+
+print("IP addresses:", iplist[3], ", and", iplist[4])
+
 print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
+
