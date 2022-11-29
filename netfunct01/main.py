b7e7ce021c446bd3759afc2b092799158e80350c
HutchesonJoe <hutcheson2001@yahoo.com>
2022-11-29 19:24:56 +0000

    Learning about functions

---
+++ netfunct01/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Alta3 Research || Author: RZFeeser@alta3.com"""
+
+# python3 -m pip install crayons
+import crayons
+import json
+# function to push commands
+def commandpush(devicecmd): # devicecmd==dict
+    
+    for ip in devicecmd.keys(): # looping through the dict
+        print(f'Handshaking. .. ... connecting with {ip}') # fstring
+        # we'll learn to write code that connects to devices here
+        for mycmds in devicecmd[ip]:
+            print(f'Attempting to sending command --> {mycmds}')
+            # we'll learn to write code that sends cmds to device here
+    return None
+
+def devicereboot(devicecmd):
+    for ip in devicecmd.keys():
+        print(f"Connecting to {ip}")
+        print(crayons.red("REBOOTING NOW!", bold=True))
+
+#start our main script
+def main():
+    """called at runtime"""
+
+    #dict containing IPs maped to a list of physical interfaces and their state
+    with open("devicecmd.json", "r") as devicecmdfile:
+        devicecmd = json.load(devicecmdfile) # decode the JSON from the file to pythonic data
+
+    print(f"Welcome to the {crayons.blue('Network')} Device Command Pusher") # welcome message
+
+    ## get data set
+    print("\nData set found\n") # replace with function call that reads in data from file 
+
+    ## run 
+    commandpush(devicecmd) # call function to push commands to devices
+    devicereboot(devicecmd)
+# call our main function 
+main()
